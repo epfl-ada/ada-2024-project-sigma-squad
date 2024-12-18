d7767b3fd25a0907d600d673484ddcdfb4ff1742
maelstuder <mael.studer@epost.ch>
2024-12-18 10:28:58 +0100

    added birth region

--- src/data/actor_data_completion.py
+++ src/data/actor_data_completion.py
@@ -29,6 +29,62 @@ def separate_dob_into_year_month(actor_df):
     return
 
 
+def groupby_region(actor_df):
+    regions = ['USA', 'United Kingdom', 'Europe', 'nan']
+    region_list = [('US', 'USA'),
+        ('United States', 'USA'),
+        ('Texas', 'USA'),
+        ('South Carolina', 'USA'),
+        ('Michigan', 'USA'),
+        ('Illinois', 'USA'),
+        ('Alabama', 'USA'),
+        ('Los Angeles', 'USA'),
+        ('Indianapolis', 'USA'),
+        ('New York', 'USA'),
+        ('Virginia', 'USA'),
+        ('New Jersey', 'USA'),
+        ('Brooklyn', 'USA'),
+        ('Tennessee', 'USA'),
+        ('Pennsylvania', 'USA'),
+        ('Chicago', 'USA'),
+        ('Nebraska', 'USA'),
+        ('Florida', 'USA'),
+        ('Ohio', 'USA'),
+        ('Scotland', 'United Kingdom'),
+        ('England', 'United Kingdom'),
+        ('Wales', 'United Kingdom'),
+        ('Ireland', 'United Kingdom'),
+        ('Berkshire', 'United Kingdom'),
+        ('Frankfurt', 'Europe'),
+        ('Germany', 'Europe'),
+        ('Spain', 'Europe'),
+        ('Netherlands', 'Europe'),
+        ('Amsterdam', 'Europe'),
+        ('France', 'Europe'),
+        ('Iceland', 'Europe'),
+        ('Italy', 'Europe'),
+        ('Bulgaria', 'Europe'),
+        ('Sweden', 'Europe'),
+        ('Bosnia', 'Europe'),
+        ('Croatia', 'Europe'),
+        ('Denmark', 'Europe'),
+        ('Slovakia', 'Europe'),]
+
+    actor_df['Birth Region'] = actor_df['Birth City'].astype(str)
+
+    for birth_city, region in region_list:
+        actor_df.loc[actor_df['Birth Region'].str.contains(birth_city, case=False), 'Birth Region'] = region
+
+    actor_df['Birth Region'] = actor_df['Birth Region'].apply(lambda x: x if x in regions else 'Rest of World')
+
+    # Fill in missing values with USA if citizenship is USA
+    actor_df.loc[actor_df['Birth Region'].str.contains('nan') & actor_df['Citizenship'].notna(), 'Birth City'] = 'USA'
+
+    #actor_df.drop(columns=['Birth City', 'Citizenship'], inplace=True)
+        
+    return
+
+
 def clean_actor_data(actor_df):
 
     actor_df.rename(columns={'Actor date of birth': 'Date of Birth',
@@ -41,6 +97,7 @@ def clean_actor_data(actor_df):
     
     convert_ethnicity_ids(actor_df)
     separate_dob_into_year_month(actor_df)
+    groupby_region(actor_df)
 
     actor_df['Age at First Release'] = actor_df['Age at First Release'].astype('Int64')
     actor_df['Theater'] = actor_df['Theater'].apply(lambda x: True if (x == 'Yes') else False)
