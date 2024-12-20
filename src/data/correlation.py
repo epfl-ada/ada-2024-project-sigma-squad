46b591d95adfdaa83c19296fd5433581d5db5ada
maelstuder <mael.studer@epost.ch>
2024-12-20 23:30:37 +0100

    changed correlation.py

--- src/data/correlation.py
+++ src/data/correlation.py
@@ -4,25 +4,36 @@ import pandas as pd
 import numpy as np
 from src.constants import ETHNICITY_MAPPING
 
-
 def encode_categorical(df, columns):
     """
     Encodes categorical features in the given DataFrame using one-hot encoding.
     """
     return pd.get_dummies(df, columns=columns)
 
-
 def display_correlation(df, plot=False):
     """
     Calculate and display the correlation of all features with the 'Success Score' in the given DataFrame.
     """
+    # Select only numeric columns
     numeric_df = df.select_dtypes(include=[np.number])
-    ## Correlation of Sucess score with other features
-    corr = df.corr()
-    corr_success = corr['Success Score']
-    corr_success = corr_success.sort_values(ascending=False)
-    corr_success = corr_success.drop('Success Score')
-    return corr_success
+    
+    # Correlation of Success score with other features
+    corr = numeric_df.corr()
+    
+    if 'Success Score' in corr.columns:
+        corr_success = corr['Success Score']
+        corr_success = corr_success.sort_values(ascending=False)
+        corr_success = corr_success.drop('Success Score')
+        
+        if plot:
+            plt.figure(figsize=(10, 8))
+            sns.heatmap(corr, annot=True, cmap='coolwarm')
+            plt.show()
+        
+        return corr_success
+    else:
+        print("Column 'Success Score' not found in the DataFrame.")
+        return None
 
 def ethnicity_to_group(df):
     """
@@ -31,5 +42,3 @@ def ethnicity_to_group(df):
     df.Ethnicity = df.Ethnicity.map(ETHNICITY_MAPPING)
     df = df.dropna(subset=['Ethnicity'])
     return df
-
-
